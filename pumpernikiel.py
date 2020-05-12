2b66a3825e4118dd956b4bdf6069ff33ae68108d
KatarzynaWatroba <64368089+KatarzynaWatroba@users.noreply.github.com>
2020-05-12 16:44:51 +0000

    Add files via upload

---
+++ pumpernikiel.py
@@ -0,0 +1,7 @@
+opusc_petle = input("wpisz slowo ratunkowe: ")
+while opusc_petle == "pumpernikiel":
+    print("udalo sie opuscic petle")
+    break
+if opusc_petle != "pumpernikiel":
+    print("Wewnątrz pętli.")
+

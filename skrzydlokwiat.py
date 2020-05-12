68984e5092899f67e1c2e5dd7b3812aded3283c0
KatarzynaWatroba <64368089+KatarzynaWatroba@users.noreply.github.com>
2020-05-12 15:19:45 +0000

    Add files via upload

---
+++ skrzydlokwiat.py
@@ -0,0 +1,5 @@
+kwiatek = input("write your favourite name of flower: ")
+if kwiatek == "skrzydlokwiat":
+    print("Skrzydłokwiat jest najlepszą rośliną w historii!")
+else:
+    print("Nie, ja chcę Skrzydłokwiat...!")

5ba966327b068dc195e4384106c76f935689cded
KatarzynaWatroba <64368089+KatarzynaWatroba@users.noreply.github.com>
2020-05-12 15:11:32 +0000

    Add files via upload

---
+++ probkakodu.py
@@ -0,0 +1,8 @@
+liczba1 = input("wprowadz pierwsza liczba: ")
+liczba2 = input("wprowadz druga liczbe: ")
+
+if liczba1 > liczba2:
+    wiekszaLiczba = liczba1
+else:
+    wiekszaLiczba= liczba2
+print("wieksza liczba to" , wiekszaLiczba)
